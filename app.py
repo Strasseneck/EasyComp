610cd9383ef069f3c1aaf7f4bc771590e5471449
strasseneck <strasseneck@gmail.com>
2023-03-06 15:55:04 +0100

    Create app, helpers and README files

---
+++ app.py
@@ -0,0 +1,12 @@
+
+# import libraries etc
+import os
+
+from cs50 import SQL
+import datetime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# import helper files
