26a6ef0bbb61f83e56875f5f6367efe6f33dc606
strasseneck <strasseneck@gmail.com>
2023-03-31 19:53:02 +0100

    So many changes. Restructure whole site

--- app.py
+++ app.py
@@ -35,7 +35,97 @@ def after_request(response):
     return response
 
 
-# index
+## ROUTE FOR ALL USERS ##
+
+# login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    # forget user_id
+    session.clear()
+
+    # user reached via POST
+    if request.method == "POST":
+
+        # ensure username
+        if not request.form.get("username"):
+            return apology("username required", 403)
+        
+        # ensure password
+        if not request.form.get("password"):
+            return apology ("password required", 403)
+        
+        # query db for user
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # ensure username exist and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+        
+        # store user_id as session user_id
+        session["user_id"] = rows[0]["id"]
+
+        # redirect to front page
+        return redirect("/")
+    
+    # user reached via GET
+    else:
+        return render_template("login.html")
+
+# log out
+@app.route("/logout")
+def logout():
+
+    # forget user_id
+    session.clear()
+
+    # redirect to login
+    return redirect("/")
+
+# Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    
+    # if reached via POST
+    if request.method == "POST":
+
+        # check for username creation
+        if not request.form.get("username"):
+            return apology("username creation required", 400)
+        
+        # check for duplicate username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows) > 0:
+            return apology("username already taken", 400)
+        
+        # check for password creation
+        elif not request.form.get("password"):
+            return apology("password creation required", 400)
+        
+        # check for password match via confirmation
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+        
+        # hash password
+        password = request.form.get("password")
+        hash = generate_password_hash(password)
+
+        # get values for db
+        username = request.form.get("username")
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+
+        rows = db.execute("INSERT INTO users (username, hash, firstname, lastname) VALUES (?,?,?,?)",
+                           username, hash, firstname, lastname)
+        
+        # redirect to log in
+        return redirect("/")
+
+    # if reached by get
+    else:
+        return render_template("register.html")
+
+# index / user profile page
 @app.route("/")
 @login_required
 def index():
@@ -85,110 +175,37 @@ def index():
 
     # render template
     return render_template("index.html", name=name, wins=wins, submissionrate=submissionrate, gold=gold, silver=silver, results=results, competitions=competitions, entered=entered)
-   
-# create competition
-@app.route("/create", methods=["GET", "POST"])
+
+# competitions
+@app.route("/competitions", methods=["GET"]) 
 @login_required
-def create():
-
-    # if reached via post
-    if request.method == "POST":
-
-        # check for compname
-        if not request.form.get("compname"):
-            return apology("competition name creation required", 400)
-        
-        # check for duplicate compname
-        rows = db.execute("SELECT * FROM competitions WHERE name = ?", request.form.get("compname"))
-        if len(rows) > 0:
-            return apology("competition with same name already exists", 400)
-        else:
-            compname = request.form.get("compname")
-        
-        # get format 
-        format = request.form["format-select"]
-        if format == 'singleroundko':
-            format = 'Single Round Elimination'
-        elif format == 'roundrobin':
-            format = 'Round Robin'
-
-        # add new competition to competitions db
-        rows = db.execute("INSERT INTO competitions (name, format, organizer_id) VALUES (?,?,?)", compname, format, session["user_id"] )
-
-        # get comp_id for divisions db
-        rows = db.execute("SELECT DISTINCT id FROM competitions WHERE name = ?", compname)
-        comp_id = rows[0]["id"]
-
-         # get belt classes
-        beltclasses = request.form.getlist("beltdivision")
-        
-        # get weight classes
-        weightclasses = request.form.getlist("weightclass")
-
-        # iterate through lists and add to divisions database
-        for i in range(len(beltclasses)):
-            for j in range(len(weightclasses)):
-                division = beltclasses[i] + weightclasses[j]
-                division = division.replace("-"," ")
-                rows = db.execute("INSERT INTO divisions (name, comp_id, comp_name) VALUES (?,?,?)", division, comp_id, compname)
-        
-        return redirect("/")
-     # if reached by get
-    else:
-        return render_template("create-comp.html")
-
-# view competition
-@app.route("/view", methods=["GET"])
-@login_required
-def view():
-
+def competitions():
+    
     # get all competitions
     competitions = db.execute("SELECT DISTINCT name, format FROM competitions")
 
     # render template
-    return render_template("view-comp.html", competitions=competitions)
+    return render_template("competitions.html", competitions=competitions)
 
-# view registrations for competition
-@app.route("/registrations/<competition>", methods=["GET"])
+# competition page
+@app.route("/competition/<name>", methods=["GET"])
 @login_required
-def registrations(competition):
+def competition(name):
 
-    #get id and name of competition
-    rows = db.execute("SELECT DISTINCT id, name FROM competitions WHERE name LIKE ?", competition)
+    # get comp_id
+    rows = db.execute("SELECT DISTINCT id FROM competitions WHERE name = ?", name)
     comp_id = rows[0]["id"]
-    compname = rows[0]["name"]
-    
-    divisions = {}
-    divnames = []
-    divids = []
-    # get division name and id
-    rows = db.execute("SELECT DISTINCT id, name FROM divisions INNER JOIN competitors on competitors.division_id = divisions.id WHERE competition_id = ?", comp_id)
-    for i in range(len(rows)):
-        divname = rows[i]["name"]
-        divnames.append(divname)
-        divid = rows[i]["id"] 
-        divids.append(divid)
 
-    # get competitors names for each division
-    for i in range(len(divids)):
-        competitors = []
-        divid = divids[i] 
-        divname = divnames[i]
-        rows = db.execute("SELECT DISTINCT firstname, lastname FROM users INNER JOIN competitors on competitors.competitor_id = users.id WHERE division_id = ?", divid)
-        for j in range(len(rows)):
-            competitor = (rows[j]["firstname"] + " " + rows[j]["lastname"])
-            competitors.append(competitor)
-        # add values to divisions dict        
-        divisions[divname] = competitors
-    
-    # check if brackets available
-    matches = db.execute("SELECT DISTINCT id, div_name, competitor1_name, competitor2_name FROM matches WHERE comp_id = ?", comp_id)
-    if len(matches) > 0:
-        return render_template("view-comp-registrations.html", compname=compname, divisions=divisions, divnames=divnames, matches=matches)
-    else:
-        return render_template("view-comp-registrations.html", compname=compname, divisions=divisions, divnames=divnames)
+    # get comp info
+    rows = db.execute("SELECT DISTINCT info FROM competitions WHERE id = ?", comp_id)
+    info = rows[0]["info"]
 
-# manage competition
+    # render template
+    return render_template("competition.html", info=info, name=name)
+
+## ROUTES FOR ORGANIZERS ##
+
+# manage competition as organizer
 @app.route("/manage", methods = ["GET"])
 @login_required
 def manage():
@@ -222,9 +239,9 @@ def manage():
         divisions[divname] = competitors
 
     #render template
-    return render_template("manage-comp.html", divisions=divisions, competitions=competitions)
+    return render_template("manage.html", divisions=divisions, competitions=competitions)
 
-# start  competition
+# start  competition as organizer
 @app.route("/start/<competition>", methods=["GET", "POST"])
 @login_required
 def start(competition):
@@ -270,7 +287,6 @@ def start(competition):
             # add values to divisions dict        
             divisions[divname] = competitors
 
-
         # master loop for each division
         for i in range(len(divisions)):
             participants = []
@@ -309,9 +325,9 @@ def start(competition):
                 
 
         displaydivs = db.execute("SELECT DISTINCT div_id, div_name FROM matches WHERE comp_id = ?", comp_id)
-        return render_template("brackets.html", displaydivs=displaydivs, compname=compname)
+        return render_template("matches.html", displaydivs=displaydivs, compname=compname)
     
-# start division
+# start division as organizer
 @app.route("/startdivision/<division>", methods=["POST"])
 @login_required
 def startdivision(division):
@@ -323,7 +339,7 @@ def startdivision(division):
     matches = db.execute("SELECT DISTINCT id, div_name, competitor1_name, competitor2_name FROM matches WHERE div_id = ?", division)
     return render_template("startdivision.html", matches=matches, divname=divname)
     
-# start match
+# start match as organizer
 @app.route("/match/<id>", methods=["POST"])
 @login_required
 def match(id):
@@ -333,7 +349,7 @@ def match(id):
 
     return render_template("match.html", match=match, id=id)
 
-# end match 
+# end match as organizer
 @app.route("/endmatch/<id>", methods=["POST"])   
 @login_required
 def endmatch(id):
@@ -422,39 +438,116 @@ def endmatch(id):
         results = db.execute("SELECT DISTINCT id, div_name, competitor1_name, competitor2_name, winner, method FROM matchresults WHERE comp_id = ?", comp_id)
         return render_template("results.html", matches=matches, results=results, compname=compname)
 
-# enter competition
-@app.route("/enter", methods=["GET"])
+# create competition as organizer
+@app.route("/create", methods=["GET", "POST"])
 @login_required
-def enter():
+def create():
 
-    # get info about competitions available to enter
-    competitions = db.execute("SELECT DISTINCT name, format FROM competitions")
-    
-    # render template enter comp
-    return render_template("enter-comp.html", competitions=competitions)
+    # if reached via post
+    if request.method == "POST":
 
-# view division
-@app.route("/divisions/<name>", methods=["GET"])
+        # check for compname
+        if not request.form.get("compname"):
+            return apology("competition name creation required", 400)
+        
+        # check for duplicate compname
+        rows = db.execute("SELECT * FROM competitions WHERE name = ?", request.form.get("compname"))
+        if len(rows) > 0:
+            return apology("competition with same name already exists", 400)
+        else:
+            compname = request.form.get("compname")
+
+        # getinfo
+        info = request.form.get("compinfo")
+        
+        # get format 
+        format = request.form["format-select"]
+        if format == 'singleroundko':
+            format = 'Single Round Elimination'
+        elif format == 'roundrobin':
+            format = 'Round Robin'
+
+        # add new competition to competitions db
+        rows = db.execute("INSERT INTO competitions (name, info, format, organizer_id) VALUES (?,?,?,?)", compname, info, format, session["user_id"] )
+
+        # get comp_id for divisions db
+        rows = db.execute("SELECT DISTINCT id FROM competitions WHERE name = ?", compname)
+        comp_id = rows[0]["id"]
+
+         # get belt classes
+        beltclasses = request.form.getlist("beltdivision")
+        
+        # get weight classes
+        weightclasses = request.form.getlist("weightclass")
+
+        # iterate through lists and add to divisions database
+        for i in range(len(beltclasses)):
+            for j in range(len(weightclasses)):
+                division = beltclasses[i] + weightclasses[j]
+                division = division.replace("-"," ")
+                rows = db.execute("INSERT INTO divisions (name, comp_id, comp_name) VALUES (?,?,?)", division, comp_id, compname)
+        
+        return redirect("/")
+     # if reached by get
+    else:
+        return render_template("create-comp.html")
+
+## ROUTES FOR COMPETITORS ##
+
+# view divisions and registrations as competitor
+@app.route("/registrations/<name>")
 @login_required
-def divisions(name):
+def registrations(name):
+
+    # get comp_id
+    rows = db.execute("SELECT DISTINCT id FROM competitions WHERE name = ?", name)
+    comp_id = rows[0]["id"]
+
+    # division objects 
+    divisions = {}
+    divnames = []
+    divids = []
+
+    # get division names and ids
+    rows = db.execute("SELECT DISTINCT id, name FROM divisions WHERE comp_id = ?", comp_id)
+    for i in range(len(rows)):
+        divname = rows[i]["name"]
+        divnames.append(divname)
+        divid = rows[i]["id"] 
+        divids.append(divid)
+        divisions[divname] = []
+
+    # get competitors names for each division
+    for i in range(len(divids)):
+        competitors = []
+        divid = divids[i] 
+        divname = divnames[i]
+        rows = db.execute("SELECT DISTINCT firstname, lastname FROM users INNER JOIN competitors on competitors.competitor_id = users.id WHERE division_id = ?", divid)
+        if len(rows) != 0:
+            for j in range(len(rows)):
+                competitor = (rows[j]["firstname"] + " " + rows[j]["lastname"])
+                competitors.append(competitor)
+            # add values to divisions dict        
+            divisions[divname] = competitors
+        
+    # render template
+    return render_template("registrations.html", divisions=divisions, name=name)
+
+# view brackets as competitor
+@app.route("/brackets/<name>", methods=["GET"])
+@login_required
+def brackets(name):
 
     #get id and name of competition
-    rows = db.execute("SELECT DISTINCT id, name FROM competitions WHERE name LIKE ?", name)
+    rows = db.execute("SELECT DISTINCT id FROM competitions WHERE name LIKE ?", name)
     comp_id = rows[0]["id"]
-    compname = rows[0]["name"]
     
-    # get division info
-    divisions = []
-    rows = db.execute("SELECT DISTINCT name FROM divisions WHERE comp_id = ?", comp_id)
-    for i in range(len(rows)):
-        division = rows[i]["name"]
-        divisions.append(division)
-    
-    # render template
-    return render_template("divisions.html", divisions=divisions, compname=compname)
+    # get brackets
+    matches = db.execute("SELECT DISTINCT id, div_name, competitor1_name, competitor2_name FROM matches WHERE comp_id = ?", comp_id)
+    return render_template("brackets.html", matches=matches, name=name)
 
-# enter division
-@app.route("/divisions/enterdivision/<name>", methods=["GET"])
+# enter division as competitor
+@app.route("/enterdivision/<name>", methods=["GET"])
 @login_required
 def enterdivision(name):
 
@@ -479,93 +572,5 @@ def enterdivision(name):
                     competitor_id, comp_id, id)
     return redirect("/")
 
-# login
-@app.route("/login", methods=["GET", "POST"])
-def login():
-
-    # forget user_id
-    session.clear()
-
-    # user reached via POST
-    if request.method == "POST":
-
-        # ensure username
-        if not request.form.get("username"):
-            return apology("username required", 403)
-        
-        # ensure password
-        if not request.form.get("password"):
-            return apology ("password required", 403)
-        
-        # query db for user
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # ensure username exist and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-        
-        # store user_id as session user_id
-        session["user_id"] = rows[0]["id"]
-
-        # redirect to front page
-        return redirect("/")
-    
-    # user reached via GET
-    else:
-        return render_template("login.html")
-
-# log out
-@app.route("/logout")
-def logout():
-
-    # forget user_id
-    session.clear()
-
-    # redirect to login
-    return redirect("/")
-
-
-# Register
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    
-    # if reached via POST
-    if request.method == "POST":
-
-        # check for username creation
-        if not request.form.get("username"):
-            return apology("username creation required", 400)
-        
-        # check for duplicate username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-        if len(rows) > 0:
-            return apology("username already taken", 400)
-        
-        # check for password creation
-        elif not request.form.get("password"):
-            return apology("password creation required", 400)
-        
-        # check for password match via confirmation
-        elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("passwords do not match", 400)
-        
-        # hash password
-        password = request.form.get("password")
-        hash = generate_password_hash(password)
-
-        # get values for db
-        username = request.form.get("username")
-        firstname = request.form.get("firstname")
-        lastname = request.form.get("lastname")
-
-        rows = db.execute("INSERT INTO users (username, hash, firstname, lastname) VALUES (?,?,?,?)",
-                           username, hash, firstname, lastname)
-        
-        # redirect to log in
-        return redirect("/")
-
-    # if reached by get
-    else:
-        return render_template("register.html")
 
 
